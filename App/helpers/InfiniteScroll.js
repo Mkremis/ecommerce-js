7aa79e40bb08589b6a93445a37b3e2bb981c93f5
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-04 14:15:55 -0300

    Add files via upload

---
+++ App/helpers/InfiniteScroll.js
@@ -0,0 +1,28 @@
+import { urlRoute } from "../urlRoute.js";
+
+urlRoute;
+export function infiniteScroll() {
+  const $scrollTopBtn = document.querySelector(".scroll-top-btn");
+
+  window.addEventListener("scroll", (e) => {
+    let { scrollTop, clientHeight, scrollHeight } = document.documentElement,
+      offset = sessionStorage.getItem("offset");
+    const { itemCount } = product;
+
+    if (scrollTop > 400) {
+      $scrollTopBtn.classList.remove("--invisible");
+    } else {
+      $scrollTopBtn.classList.add("--invisible");
+    }
+
+    if (scrollTop + clientHeight > scrollHeight && offset) {
+      offset = parseInt(offset);
+      let rest = itemCount - offset;
+      offset += Math.min(48, rest);
+      sessionStorage.setItem("offset", offset);
+      if (offset < itemCount) urlRoute(window.location.pathname);
+    } else {
+      return false;
+    }
+  });
+}
