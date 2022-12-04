8ba9fbc7ef38f015513c1ad101291a8a575c0ff6
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-04 16:56:57 -0300

    Update topBtn.js

--- App/components/topBtn.js
+++ App/components/topBtn.js
@@ -1,6 +1,5 @@
 export function TopButton() {
   const $scrollTopButton = document.createElement("button");
   $scrollTopButton.classList.add("scroll-top-btn", "--invisible");
-  $scrollTopButton.innerHTML = "<img src='/eCommerce-SPA/images/up.svg'>";
   return $scrollTopButton;
 }
