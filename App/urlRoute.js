996d727065e518fb958870968ead32174b0fb9c2
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-04 14:20:31 -0300

    Update urlRoute.js

--- App/urlRoute.js
+++ App/urlRoute.js
@@ -1,14 +1,43 @@
 import { urlHandler } from "./urlHandler.js";
 
 export function urlRoute(event) {
+  let { href } = window.location,
+    { pathname } = window.location;
   if (event === window.event) {
-    localStorage.removeItem("sort");
     event = event || window.event;
     event.preventDefault();
     window.history.pushState({}, "", event.target.href);
+    if (event.target.href !== href) {
+      if (
+        event.target.href.includes("/search") ||
+        event.target.href.includes("/women") ||
+        event.target.href.includes("/men") ||
+        event.target.href.includes("/sneakers")
+      ) {
+        offsetInit();
+      } else {
+        sessionStorage.removeItem("offset");
+      }
+    }
   } else {
     window.history.pushState({}, "", event);
+    if (event !== pathname) {
+      if (
+        pathname.includes("/search") ||
+        pathname.includes("/women") ||
+        pathname.includes("/men") ||
+        pathname.includes("/sneakers")
+      ) {
+        offsetInit();
+      } else {
+        sessionStorage.removeItem("offset");
+      }
+    }
   }
-
+  function offsetInit() {
+    sessionStorage.setItem("offset", 0);
+    sessionStorage.removeItem("filter");
+    sessionStorage.removeItem("menufilter");
+  }
   urlHandler();
 }
