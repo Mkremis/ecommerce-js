f41fd1324ed9e30d163b1603072bd32bfe41872f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-04 17:58:29 -0300

    Update Sort.js

--- App/helpers/Sort.js
+++ App/helpers/Sort.js
@@ -4,6 +4,7 @@ export function Sort() {
   const $sort = document.getElementById("sortBy");
   $sort.addEventListener("change", (e) => {
     localStorage.setItem("sort", e.target.value);
+     document.getElementById("content").innerHTML = null;
     urlRoute(window.location.pathname);
   });
 }
