49b79c3a52b21477a9753ac29bbc3e1d841ed34f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-04 14:28:51 -0300

    Update HomeLand.js

--- App/components/HomeLand.js
+++ App/components/HomeLand.js
@@ -9,12 +9,12 @@ export function Home(women, men) {
 
   let bestWM, bestM;
   const options = {
-     method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1693f38f67mshed9ca24b1b2aec1p1fe1b4jsnd9da89021c3a',
-		'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
-	}
-},
+     method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "2396f3e6fdmsh5a90589f99d3620p11ae3cjsn28439e10058d",
+        "X-RapidAPI-Host": "asos2.p.rapidapi.com",
+      },
+    },
     urlWM = `https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=${women}&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US`,
     urlM = `https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=${men}&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US`,
     $WMfragment = document.createDocumentFragment(),
