27820a37b62fe2905b663a2f1ab681ed803d41d0
Mkremis <105755654+Mkremis@users.noreply.github.com>
2022-12-05 11:52:37 -0300

    Update Ajax.js

--- App/helpers/Ajax.js
+++ App/helpers/Ajax.js
@@ -1,11 +1,11 @@
 export function ajax({ url, success }) {
   const options = {
-  method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "2396f3e6fdmsh5a90589f99d3620p11ae3cjsn28439e10058d",
-        "X-RapidAPI-Host": "asos2.p.rapidapi.com",
-      },
-    };
+  method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd210c99c19mshfe716f01deb4167p14f4c9jsn9e4d8f8900ce',
+		'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
+	}
+};
 
   const FETCH = fetch(url, options)
     .then((res) => res.json())
