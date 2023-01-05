376c441b50cfc70b47740c9b6e24bbad01ae792c
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-01-05 19:27:55 -0300

    test

--- App/index.js
+++ App/index.js
@@ -134,6 +134,7 @@ document.addEventListener("click", (e) => {
 
 document.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(e.target);
   if (e.target.matches(".login-form")) Login(new FormData(e.target));
   if (e.target.matches(".user-account__form-submit")) console.log(e.target);
 });
