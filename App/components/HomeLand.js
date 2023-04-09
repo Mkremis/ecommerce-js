604a73d8495385f997eb2c1b94f5224daf8ffa8b
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-04-09 11:50:34 -0300

    Update HomeLand.js

--- App/components/HomeLand.js
+++ App/components/HomeLand.js
@@ -6,10 +6,10 @@ export function Home(women, men) {
   SubCat();
   const $loader = document.getElementById("loader"),
     $mobileNav = document.querySelector(".modal-navbar__background ");
-
+console.log(women);
   let bestWM, bestM;
   const options = {
-   method: 'GET',
+  method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'd210c99c19mshfe716f01deb4167p14f4c9jsn9e4d8f8900ce',
 		'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
